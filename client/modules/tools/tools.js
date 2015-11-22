c076f640963275dda5d9b91a4e701f8ff9b46c48
searleb <billsearle@me.com>
2015-11-22 19:45:42 +1100

    loop for tools from google sheets

--- client/modules/tools/tools.js
+++ client/modules/tools/tools.js
@@ -1,21 +1,31 @@
 Template.tools.onCreated( function(){
+
+    var tabIDs = {
+        'developers': 'ozcs9yb',
+        'designers': 'od6',
+        'accounts': 'oq5732i',
+        'managers': 'oluqtiy'
+    };
+    var tools = {};
+
     // google sheets has an ID for each tab, found here, search for 'full/':
     // https://spreadsheets.google.com/feeds/worksheets/1i8WWy8BWOZXVDATgLAicRzRdi3i8wmv00ZMsEerdFQ8/private/full
-    // var spreadsheetID = "1i8WWy8BWOZXVDATgLAicRzRdi3i8wmv00ZMsEerdFQ8",
-    // url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + '/' + worksheetID + "/public/full?alt=json";
-    // console.log("tools " url);
-    // $.getJSON(url, function(data) {
-    //     if (data) {
-    //         hsh.tools.showModule('tools');
-    //         var entry = data.feed.entry;
-    //         // console.log(data);
-    //
-    //         $(entry).each(function(){
-    //             // Column names are title, link, desc.
-    //             $('.tools .content .list-container ul').append('<li style="display:none;"><a target="_blank" href=http://' + this.gsx$link.$t + '>' + this.gsx$title.$t + '</a><span class="type">'+ this.gsx$desc.$t + '</span><span class="clearfix"></span></li>');
-    //         }).promise().done(function(){
-    //             $('.tools .content .list-container ul li').fadeIn(200);
-    //         });
-    //     };
-    // });
+    $.each( tabIDs, function(key, value) {
+        // console.log(key, value);
+        var spreadsheetID = "1i8WWy8BWOZXVDATgLAicRzRdi3i8wmv00ZMsEerdFQ8",
+        url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/" + value + "/public/full?alt=json";
+        $.getJSON(url, function(data) {
+            if (data) {
+                var entry = data.feed.entry;
+                tools[key] = entry;
+                $('.tools .content .list-container').append('<ul class="' + key + '"><h4>' + key + '</h4>');
+                $(entry).each(function(){
+                    // Column names are title, link, desc.
+                    $('.tools .content .list-container ul.' + key).append('<li style="display:none;"><a target="_blank" href=http://' + this.gsx$link.$t + '>' + this.gsx$title.$t + '</a><span class="type">'+ this.gsx$desc.$t + '</span></li>');
+                }).promise().done(function(){
+                    $('.tools .content .list-container ul li').fadeIn(200);
+                });
+            }
+        });
+    });
 });
