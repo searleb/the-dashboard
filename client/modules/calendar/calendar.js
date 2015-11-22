328d515e1cb1ca6fc6d9dc9e06d5299c91682798
searleb <billsearle@me.com>
2015-11-22 19:44:07 +1100

    calendar tidy and gitignore

--- client/modules/calendar/calendar.js
+++ client/modules/calendar/calendar.js
@@ -10,19 +10,19 @@ Template.calendar.onCreated( function () {
     console.log('on created');
     this.autorun(function(){
         userData = Meteor.user();
-        console.log('userData is:', typeof(userData), userData);
+        // console.log('userData is:', typeof(userData), userData);
         if ( typeof(userData) == 'object' && userData != null) {
             getCalendar();
         }
     });
 
     function getCalendar(argument) {
-        console.log('getCalendar');
+        // console.log('getCalendar');
         var timeMin = new moment().set('hour', 0).set('minute', 0).set('second', 0),
             timeMax = new moment().add(1,'days').set('hour', 0).set('minute', 0).set('second', 0),
             url = 'calendar/v3/calendars/' + userData.services.google.email + '/events/?timeMin=' + timeMin.toISOString() + '&timeMax=' + timeMax.toISOString() + '&maxResults=10' + '&orderBy=startTime' + '&singleEvents=true' + '&access_token=' + userData.services.google.accessToken;
         GoogleApi.get(url, {}, function(error, data){
-            console.log(data);
+            // console.log(data);
             for (var i in data.items) {
                 var startTime = moment(data.items[i].start.dateTime).format('hh:mma'),
                     endTime = moment(data.items[i].end.dateTime).format('hh:mma');
