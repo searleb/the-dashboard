da749709fd38dd290c86c7b644f9af5943813777
searleb <billsearle@me.com>
2016-01-05 18:31:00 +1100

    ui unstyled accounts, moved constants.js into server side

--- server/medium-server.js
+++ server/medium-server.js
@@ -1,10 +1,6 @@
-Meteor.startup(function () {
-    // // code to run on server at startup
-    Meteor.methods({
-        getMediumFeed: function(){
-            data = Scrape.feed("http://medium.com/feed/@MentallyFriendly");
-            return data;
-        }
-    });
-
+Meteor.methods({
+    getMediumFeed: function(){
+        data = Scrape.feed("http://medium.com/feed/@MentallyFriendly");
+        return data;
+    }
 });
--- server/robin-feed-server.js
+++ server/robin-feed-server.js
@@ -1,14 +1,12 @@
-Meteor.startup(function(){
-    Meteor.methods({
-        getRobinRooms: function(){
-            var robinURL = "https://api.robinpowered.com/v1.0/";
-            console.log("ROBIN URL " + robinURL);
-            var auth = HTTP.get(robinURL + "spaces/1", {
-                auth: ROBIN_APP_TOKEN
-            });
-            console.log("AUTH ", auth);
-            return auth;
+Meteor.methods({
+    getRobinRooms: function(){
+        var robinURL = "https://api.robinpowered.com/v1.0/";
+        console.log("ROBIN URL " + robinURL);
+        var auth = HTTP.get(robinURL + "spaces/1", {
+            auth: ROBIN_APP_TOKEN
+        });
+        console.log("AUTH ", auth);
+        return auth;
 
-        }
-    });
+    }
 });
