2871145d32ff6d85aa8464ade5e0fdf8cbc5f129
searleb <billsearle@me.com>
2016-01-06 17:14:03 +1100

    fixed sidebar tooltips on login

--- client/modules/sidebar/sidebar.js
+++ client/modules/sidebar/sidebar.js
@@ -1,7 +1,16 @@
+// Run the tooltips when the template is rendered.
 Template.sidebar.onRendered(function (argument) {
     $('.iconav [data-toggle="tooltip"]').tooltip();
 });
 
+// If the user is not logged in, wait 500ms after login to initialize the hidden tooltips.
+Tracker.autorun(function(){
+    if(Meteor.userId()){
+        setTimeout(function () {
+            $('.iconav [data-toggle="tooltip"]').tooltip();
+        }, 500);
+    }
+});
 
 Template.sidebar.helpers({
     userData: function(){
