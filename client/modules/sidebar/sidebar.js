d64ea82ab688f1728663e14d7653329713e468da
searleb <billsearle@me.com>
2016-01-07 11:38:25 +1100

    updated sidebar to use if currentUser

--- client/modules/sidebar/sidebar.js
+++ client/modules/sidebar/sidebar.js
@@ -3,15 +3,6 @@ Template.sidebar.onRendered(function (argument) {
     $('.iconav [data-toggle="tooltip"]').tooltip();
 });
 
-// If the user is not logged in, wait 500ms after login to initialize the hidden tooltips.
-Tracker.autorun(function(){
-    if(Meteor.userId()){
-        setTimeout(function () {
-            $('.iconav [data-toggle="tooltip"]').tooltip();
-        }, 500);
-    }
-});
-
 Template.sidebar.helpers({
     userData: function(){
         return Meteor.user();
