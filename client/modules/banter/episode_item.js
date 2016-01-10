382b60e08b04831d2758f36d0f5a89a8eeee8303
Robert <robert@rokocodesign.com>
2016-01-11 10:53:24 +1100

    Templating Episodes
    Removed the static episode markup and set up template with and static
    episode data

---
+++ client/modules/banter/episode_item.js
@@ -0,0 +1,10 @@
+var episodeData = [
+  {
+    number: 'Episode One',
+    title: 'Flight of the Condor',
+    info: 'In this episode we talk all about Data, BrandData with Liam, and also ponys and other majestic creatures.'
+  }
+];
+Template.friendlyBanter.helpers({
+  episode: episodeData
+});
