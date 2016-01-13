ccf22c1f3d5704e7706279d4827635fbddc7bcc3
searleb <billsearle@me.com>
2016-01-13 11:15:15 +1100

    rolled user auth

--- client/config.js
+++ client/config.js
@@ -1,16 +1,3 @@
-// Login options
-Accounts.ui.config({
-    requestPermissions:{
-        google:['https://www.googleapis.com/auth/calendar', 'email', 'openid']
-    },
-    requestOfflineToken: {
-        google: true
-    },
-    forceApprovalPrompt: {
-        google: true
-    }
-});
-
 // restrict signups to only @mentallyfriendly.com email addresses
 Accounts.config({
     restrictCreationByEmailDomain: 'mentallyfriendly.com'
--- server/accounts-server.js
+++ server/accounts-server.js
@@ -1,26 +1,30 @@
 // restrict signups to only @mentallyfriendly.com email addresses
 Meteor.startup(function () {
-    Accounts.config({
-        restrictCreationByEmailDomain: 'mentallyfriendly.com'
-    });
+   Accounts.config({
+      restrictCreationByEmailDomain: 'mentallyfriendly.com'
+   });
 });
 
 Meteor.methods({
-    'saveUserOffice': function(office) {
-        var user = Meteor.user();
-        Meteor.users.update({_id: Meteor.user()._id}, {$set: {"profile.office": office}});
-    },
-    'saveUserRole': function(role) {
-        var user = Meteor.user();
-        Meteor.users.update({_id: Meteor.user()._id}, {$set: {"profile.role": role}});
-    }
+   'saveUserOffice': function(office) {
+      var user = Meteor.user();
+      Meteor.users.update({_id: Meteor.user()._id}, {$set: {"profile.office": office}});
+   },
+   'saveUserRole': function(role) {
+      var user = Meteor.user();
+      Meteor.users.update({_id: Meteor.user()._id}, {$set: {"profile.role": role}});
+   }
 });
 
 Meteor.publish("userData", function () {
-    if (this.userId) {
-        return Meteor.users.find({_id: this.userId},
-            { fields: {'services.google.picture': 1} });
-    } else {
-        this.ready();
-    }
-});
+   if (this.userId) {
+      return Meteor.users.find({_id: this.userId},
+         { fields: {
+            'services.google.picture': 1,
+            'services.google.accessToken': 1,
+            'services.google.email': 1 }
+         });
+      } else {
+         this.ready();
+      }
+   });
