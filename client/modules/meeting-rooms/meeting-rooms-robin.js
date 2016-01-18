98421adec1b05bf3cabf5b84e7860b703509905b
searleb <billsearle@me.com>
2016-01-18 12:04:20 +1100

    refactored progress and in session functions - hopefully fixing performance issue

--- client/modules/meeting-rooms/meeting-rooms-robin.js
+++ client/modules/meeting-rooms/meeting-rooms-robin.js
@@ -1,69 +1,100 @@
 Template.meetingRooms.onCreated(function(){
+   // onCreated we call for all the rooms details and set it into Session
    function getRooms() {
-      console.log('getRooms');
       var robinFeed = Meteor.call('getRobinRooms', function (err, data) {
          if (err) {
             Session.set('meetingRooms', err);
          } else {
             Session.set('meetingRooms', data);
-            console.log(data);
          }
       });
    }
 
    // call getRooms and update every 5 minutes
    Meteor.setInterval(function(){
-      getRooms();
+      if (tabIsFocused) {
+         getRooms();
+      }
    }, (1000 * 60) * 5);
 
    getRooms();
+});
+
+
+Template.meetingRooms.onRendered(function () {
+   // add event listener to window to check if window is in focus
+   tabIsFocused = true;
+   window.addEventListener('focus', function() {
+      tabIsFocused = true;
+   },false);
+
+   window.addEventListener('blur', function() {
+      tabIsFocused = false;
+   },false);
+
+   // updates progress bar
+   function getProgress(start, end, id){
+      var now = moment();
+      var starting = moment(start);
+      var ending = moment(end);
+
+      if (starting <= now && ending >= now) {
+         var duration = ending - starting;
+         var progress = now - starting;
+         var percentage = (progress / duration) * 100;
+
+         Session.set('progress-percent' + id, percentage);
+      }
+   }
+
+   // checks if booking is in session
+   function getIsSession(start, end, id) {
+      var now = moment();
+      var starting = moment(start);
+      var ending = moment(end);
+      var className;
+      if (starting <= now && ending >= now) {
+         Session.set('is-session-class' + id, 'in-session');
+      } else {
+         Session.set('is-session-class' + id, 'not-in-session');
+      }
+   }
+
+   // loop each rooms bookings and update the progress bar and check is in session
+   function updateBooking() {
+      var rooms = Session.get('meetingRooms');
+      _.each(rooms, function (room) {
+         _.each(room.bookings, function (booking) {
+            var start = booking.started_at,
+            end = booking.ended_at,
+            id = booking.id;
+
+            getProgress(start, end, id);
+            getIsSession(start, end, id);
+         });
+      });
+   }
+
+   Meteor.setInterval(function(){
+      if (tabIsFocused) {
+         updateBooking();
+      }
+   }, 1000 * 2);
+   updateBooking();
 
 });
+
 
 Template.meetingRooms.helpers({
    room: function () {
       return Session.get('meetingRooms');
    },
-   // returns a percentage progress of the current booking
-   progress: function(start, end, id){
-      (function getProgress(){
-         var now = moment();
-         var starting = moment(start);
-         var ending = moment(end);
-
-         if (starting <= now && ending >= now) {
-            var duration = ending - starting;
-            var progress = now - starting;
-            var percentage = progress / duration * 100;
-
-            Session.set('progress-percent' + id, percentage);
-         }
-      })();
-
-      Meteor.setInterval(function(){
-         getProgress();
-      }, 1000 * 30);
-
+   // returns a % progress number
+   progressPercent: function(id){
       return Session.get('progress-percent' + id);
    },
    // returns a class name if the booking is in session or not
-   isInSession: function (start, end, id) {
-      (function getIsSession() {
-         var now = moment();
-         var starting = moment(start);
-         var ending = moment(end);
-         var className;
-         if (starting <= now && ending >= now) {
-            Session.set('is-session-class' + id, 'in-session');
-         } else {
-            Session.set('is-session-class' + id, 'not-in-session');
-         }
-      })();
-
-      Meteor.setInterval(function () {
-         getIsSession();
-      }, 1000 * 30);
-
+   isInSession: function (id) {
       return Session.get('is-session-class' + id);
    },
    whatsFree: function () {
