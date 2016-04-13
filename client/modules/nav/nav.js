8cfae1471941966951bcd6a5cefeefa3b0a36953
Ed Corcoran <hello@ejcorcoran.com>
2016-04-13 10:41:00 +1000

    All the changes
    Template and Style changes across the board

--- client/modules/nav/nav.js
+++ client/modules/nav/nav.js
@@ -1,19 +1,4 @@
 // Run the tooltips when the template is rendered.
-Template.nav.onRendered(function (argument) {
-   $('.iconav [data-toggle="tooltip"]').tooltip();
-
-   (function isNewTab() {
-      var list = $('.iconav-slider .iconav-nav').children('li');
-      $(list).each(function(index, el) {
-         $this = $(el);
-         var attr = $(el).children('a').attr('target');
-         if (attr == '_blank') {
-            $this.addClass('new-tab');
-         }
-      });
-   })();
-});
-
 Template.nav.helpers({
    userData: function(){
       return Meteor.user();
