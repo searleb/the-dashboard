c3f79294136d6f0396e2956c63f9895c871d3fe9
searleb <billsearle@me.com>
2016-06-27 11:28:54 +1000

    Added Packery and module style updates to match

--- the-dashboard.js
+++ the-dashboard.js
@@ -1,32 +1,33 @@
 if (Meteor.isClient) {
-    // helper to convert objects to arrays for use in blaze templates
-    UI.registerHelper("arrayify", function(obj){
-        result = [];
-        for (var key in obj){
-            result.push({name:key,value:obj[key]});
-        }
-        return result;
-    });
 
-    // format time
-    UI.registerHelper('formatTime', function(time, formatting) {
-        if(formatting) {
-            return moment(time).format(formatting);
-        }
-    });
+   // helper to convert objects to arrays for use in blaze templates
+   UI.registerHelper("arrayify", function(obj){
+      result = [];
+      for (var key in obj){
+         result.push({name:key,value:obj[key]});
+      }
+      return result;
+   });
 
-    // format time
-    UI.registerHelper('dateToday', function() {
-        return moment().format('DD MMM YY');
-    });
+   // format time
+   UI.registerHelper('formatTime', function(time, formatting) {
+      if(formatting) {
+         return moment(time).format(formatting);
+      }
+   });
 
-    // add global event listener to window to check if window is in focus
-    tabIsFocused = true;
-    window.addEventListener('focus', function() {
+   // format time
+   UI.registerHelper('dateToday', function() {
+      return moment().format('DD MMM YY');
+   });
+
+   // add global event listener to window to check if window is in focus
+   tabIsFocused = true;
+   window.addEventListener('focus', function() {
       tabIsFocused = true;
-    },false);
+   },false);
 
-    window.addEventListener('blur', function() {
+   window.addEventListener('blur', function() {
       tabIsFocused = false;
-    },false);
+   },false);
 }
